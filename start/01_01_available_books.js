4b37bdfcc09af8103f9df54bae8688e54f1814bc
Ramdlan Faqih <64480525+RamdlanFaqih@users.noreply.github.com>
2023-12-06 15:37:07 +0000

    add get availbility function

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,23 @@
 // Write your code here
+
+console.log("Test");
+
+function Book(title, author, isbn, numCopies) {
+  this.title = title,
+  this.author = author,
+  this.isbn = isbn,
+  this.numCopies = numCopies
+}
+
+Book.prototype.getAvailability = function() {
+  if (this.numCopies === 0) {
+    return "not available"
+  } else if (this.numCopies < 10) {
+    return "low stock"
+  } else {
+    return "in stock"
+  }
+};
+
+const myBook = new Book("Buku", "Faqih", "test", 1);
+console.log(myBook.getAvailability());
