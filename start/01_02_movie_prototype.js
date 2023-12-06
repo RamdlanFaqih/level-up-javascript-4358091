98bbaf94919b47cd68bf0408925aa2dc5623dd28
Ramdlan Faqih <64480525+RamdlanFaqih@users.noreply.github.com>
2023-12-06 16:19:56 +0000

    add class notation of Movie

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,17 @@
 // Write your code here
+
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+   this.title = title
+   this.director = director
+   this.genre = genre
+   this.releaseYear = releaseYear
+   this.rating = rating
+  }
+  getOverview() {
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`
+  }
+}
+
+const film = new Movie('The Raid', 'Faqih', 'action', 'top', '2018')
+console.log(film.getOverview());
